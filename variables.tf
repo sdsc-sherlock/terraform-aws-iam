b0a71365dd3be57ea19a029b467bea51c6dba72a
Eric Odell <cirelledo@gmail.com>
2024-05-08 09:20:24 -0700

    dummy variables

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "nothing_to_see_here" {
+  description = "collection of iam modules, cf examples/* on howto"
+  default     = "collection of iam modules, cf examples/* on howto"
+  type        = string
+}
